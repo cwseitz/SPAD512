2c2f7a5e4e38d2f43fecc44b8864b71c11efb232
FishyIshyFishy <165215506+FishyIshyFishy@users.noreply.github.com>
2024-06-08 05:44:31 -0400

    Update stack.py
    Removed redundant lines from previous attempt

--- stack.py
+++ stack.py
@@ -3,13 +3,9 @@ import glob
 import tifffile as tiff
 
 folder = '240607/5_ms_6bit'
-images = glob.glob(folder + '/*')
-
-image = imread(image_files[0])
-x, y = image.shape
 
 with tiff.TiffWriter(folder + '.tif', bigtiff=True) as tif:
-    for image in images:
+    for image in glob.glob(folder + '/*'):
         image = imread(image)
         tif.write(image, contiguous=True)
 
