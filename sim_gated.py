95f2f73962aead8df385e09d25c7614e3cdce379
FishyIshyFishy <165215506+FishyIshyFishy@users.noreply.github.com>
2024-06-14 02:09:02 -0400

    Update sim_gated.py
    Fixed plotting (again), greatly improved runtime by vectorizing random draws from exponentials across gates.

--- sim_gated.py
+++ sim_gated.py
@@ -3,7 +3,6 @@ import time
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 from multiprocessing import Pool
 from skimage.io import imsave, imread
 
@@ -11,12 +10,15 @@ from skimage.io import imsave, imread
 Simulation of exponential fitting of fluorescent lifetime imaging data acquired by a time-gated SPAD
 '''
 
-'''Simulation Parameters'''
+'''Manipulated Simulation Parameters'''
+integ_sims = [0.001, 0.0025, 0.005, 0.0075, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 0.75, 1, 5, 10] # integration times in ms
+step_sims = [0.018, 0.04, 0.06, 0.1, 0.15, 0.2, 0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 5] # gate step sizes (not width) in ns
+tau_sims = [10, 5, 1]  # ground truth lifetimes in ns
+tau_sim = 10
+
+'''Standard Simulation Parameters'''
 freq_sim = 10  # laser frequency in MHz
-integ_sims = [0.001, 0.0025, 0.005, 0.0075, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 0.75, 1]
-step_sims = [0.018, 0.04, 0.06, 0.1, 0.15, 0.2, 0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 5]
 offset_sim = 0.018  # gate offset in ns
-tau_sim = [1, 5, 10]  # ground truth lifetime to simulate
 width_sim = 5  # gate width in nsW
 iter = 1000 # number of times to iterate for each combination of integrations and steps
 zeta_sim = 0.05 # probability of detection per pulse, from QY, detector eff., cross section, excitation flux
@@ -25,16 +27,16 @@ zeta_sim = 0.05 # probability of detection per pulse, from QY, detector eff., cr
 def genData(freq, integ, step, offset, width, tau, zeta):
     numgates = int(1e3 * freq * integ)
     interpulse = 1e3 / freq
-    numsteps = int(interpulse / step) # take floor to prevent pulse overhang
-    lam = 1/tau 
+    numsteps = int(interpulse / step)
+    lam = 1 / tau
 
     data = np.zeros(numsteps, dtype=int)
+    steps = np.arange(numsteps) * step
+    prob = zeta * (np.exp(-lam * (offset + steps)) - np.exp(-lam * (offset + steps + width)))
+    
     for i in range(numsteps):
-        prob = np.exp(-lam * (offset + i*step)) - np.exp(-lam * (offset + i*step + width))  # direct integration of PDF
-        prob = zeta * prob # prob is sim'd for single pulse, so can directly multiply by zeta
-        draws = np.random.rand(numgates) < prob
-        count = np.sum(draws)
-        data[i] += count
+        draws = np.random.rand(numgates) < prob[i]
+        data[i] = np.sum(draws)
 
     return data
 
@@ -51,12 +53,14 @@ def getLifetime(data, step):
 
         try: 
             params, cov = opt.curve_fit(decay, x, y, p0=initial_guess)
+            success = True
         except RuntimeError:
-            params = [0, 0] # not really sure how else to handle curve_fit failing after many iterations
+            params = [100, 100] # not really sure how else to handle curve_fit failing after many iterations
+            success = False
         
-        return params
+        return params, success
 
-    LMA_params = fit_decay(data, step)
+    LMA_params, success = fit_decay(data, step)
 
     # plots decay curve from a particular iteration
     # x = np.arange(len(data)) * step
@@ -66,21 +70,23 @@ def getLifetime(data, step):
     # plt.xlabel('Time, ns')
     # plt.ylabel('Counts')
     # plt.legend()
-    # plt.title('Simulated Decay for 10 ms integration, 5 ns step')
+    # plt.title('Simulated Decay for 10 us integration, 5 ns step')
     # plt.show()
 
-    return LMA_params
+    return LMA_params, success
 
 '''Inner function for multiprocessing'''
-def runSimulation(freq, integ, step, offset, width, tau):
-    data = genData(freq, integ, step, offset, width, tau)
-    info = getLifetime(data, step)
-    return info[1] # returns lifetime (tau) in nanoseconds
+def runSimulation(freq, integ, step, offset, width, tau, zeta):
+    data = genData(freq, integ, step, offset, width, tau, zeta)
+    if max(data) > 0:
+        info, success = getLifetime(data, step)
+        return info[1], success  # returns lifetime (tau) in nanoseconds and success flag
+    return 0, False
 
 '''Outer function for multiprocessing'''
 def job(n): # need to call n as a parameter for multiprocessing syntax
-    out = runSimulation(freq_sim, integ_sim, step_sim, offset_sim, width_sim, tau_sim, zeta_sim) 
-    return out
+    out, success = runSimulation(freq_sim, integ_sim, step_sim, offset_sim, width_sim, tau_sim, zeta_sim)
+    return out, success
 
 '''Data display function'''
 def display(mean_image, std_image, integs, steps, tau, show = False):
@@ -88,24 +94,25 @@ def display(mean_image, std_image, integs, steps, tau, show = False):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
     
     # plot mean
-    norm_mean = mcolors.TwoSlopeNorm(vmin=np.min(mean_image), vcenter=tau, vmax=np.max(mean_image))
-    cax1 = ax1.imshow(mean_image, cmap='PRGn', norm=norm_mean)
+    norm = mcolors.TwoSlopeNorm(vmin=np.min(mean_image), vcenter=tau, vmax=np.max(mean_image))
+    cax1 = ax1.imshow(mean_image, cmap='seismic', norm=norm)
     cbar1 = fig.colorbar(cax1, ax=ax1, shrink = 0.6)
-    cbar1.set_label('Mean of Lifetimes, ns')
-    ax1.set_title('Mean of Lifetimes')
+    cbar1.set_label('Means, ns')
+    ax1.set_title('Mean Lifetimes')
     ax1.set_xlabel('Step size (ns)')
     ax1.set_ylabel('Integration time (ms)')
     ax1.set_yticks(np.linspace(0, xlen, num=xlen, endpoint=False))
     ax1.set_yticklabels(integs)
     ax1.set_xticks(np.linspace(0, ylen, num=ylen, endpoint=False))
     ax1.set_xticklabels(steps)
-    plt.setp(ax1.get_xticklabels(), rotation=30)
+    plt.setp(ax1.get_xticklabels(), rotation=45)
 
     # plot stdevs
-    norm_std = mcolors.Normalize(vmin=0, vmax=1, clip=True)
-    cax2 = ax2.imshow(std_image, cmap='hot', norm=norm_std)
+    std_image = np.clip(std_image, -1, 2)
+    norm = mcolors.TwoSlopeNorm(vmin=-1, vcenter=0, vmax=2)
+    cax2 = ax2.imshow(std_image, cmap='seismic', norm=norm)
     cbar2 = fig.colorbar(cax2, ax=ax2, shrink = 0.6)
-    cbar2.set_label('Standard Deviation of Lifetimes, ns')
+    cbar2.set_label('St Devs, ns')
     ax2.set_title('Standard Deviation of Lifetimes')
     ax2.set_xlabel('Step size (ns)')
     ax2.set_ylabel('Integration time (ms)')
@@ -113,9 +120,10 @@ def display(mean_image, std_image, integs, steps, tau, show = False):
     ax2.set_yticklabels(integs)
     ax2.set_xticks(np.linspace(0, ylen, num=ylen, endpoint=False))
     ax2.set_xticklabels(steps)
-    plt.setp(ax2.get_xticklabels(), rotation=30)
+    plt.setp(ax2.get_xticklabels(), rotation=45)
 
-    plt.savefig(f'{tau}ns_full', bbox_inches='tight')
+    plt.savefig(str(tau) + 'ns_full', bbox_inches='tight')
+    print('Figure saved as ' + str(tau) + 'ns_full.png')
 
     if show:
         plt.tight_layout()
@@ -130,31 +138,36 @@ for tau_sim in tau_sims:
     for i, integ_sim in enumerate(integ_sims):
         for j, step_sim in enumerate(step_sims):
             tic = time.time()
-            print(f'Beginning simulation for {integ_sim} ms integration and {step_sim} ns step size.')
+            print(f'Beginning simulation for {integ_sim} ms integ, {step_sim} ns step, {tau_sim} ns lifetime.')
 
             if __name__ == '__main__': # multiprocessing bit
                 with Pool(100) as p:
-                    lifetimes = p.map(job, range(iter))
+                    results = p.map(job, range(iter))
+
+            lifetimes = [res[0] for res in results if res[1]]  # Filter out non-converging fits
 
             # box plots lifetimes for a particular integ/step combo
             # plt.figure(figsize=(6, 4))
             # plt.boxplot(lifetimes, vert=False,  patch_artist=True)
-            # plt.title('Box Plot of Lifetimes for 1 ms integration, 5 ns step')
+            # plt.title('Box Plot of Lifetimes for 10 ms integration, 100 ps step')
             # plt.xlabel('Lifetimes, in ns')
             # plt.show()
 
-            stdevs[i][j] += np.std(lifetimes) 
-            means[i][j] += np.average(lifetimes) 
-
+            if ((len(lifetimes) > 0) and (max(lifetimes) > 0)):  # Ensure there are valid lifetimes
+                stdevs[i][j] += np.std(lifetimes) 
+                means[i][j] += np.average(lifetimes) 
+            else: 
+                stdevs[i][j] = -1
+                means[i][j] = -1
             toc = time.time()
-            print(f'Simulation ns complete in {toc-tic} seconds. Lifetime stdev {stdevs[i][j]}\n')
+            print(f'Simulation complete in {toc-tic} seconds. Lifetime stdev {stdevs[i][j]}\n, mean {means[i][j]}')
     tic2 = time.time()
     print(f'Full simulation for {tau_sim} ns lifetime complete in {tic2-tic1} seconds.')
-    imsave(f'{tau_sim}ns_stdevs.tif',stdevs)
-    imsave(f'{tau_sim}ns_means.tif', means)
+    imsave(str(tau_sim) + 'ns_stdevs.tif',stdevs)
+    imsave(str(tau_sim) + 'ns_means.tif',means)
     display(means, stdevs, integ_sims, step_sims, tau_sim, show = False)
 
-# stdevs = imread('sim4_stdevs.tif')
-# means = imread('sim4_means.tif',)
+# stdevs = imread('10ns_stdevs.tif')
+# means = imread('10ns_means.tif',)
 
-display(means, stdevs, integ_sims, step_sims, tau_sim, show = False)
+# display(means, stdevs, integ_sims, step_sims, tau_sim, show = True)
