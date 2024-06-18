e0fca0c67a2c813c3a17bcbb0250b8a7fd5b0138
FishyIshyFishy <165215506+FishyIshyFishy@users.noreply.github.com>
2024-06-18 15:08:46 -0400

    Update exponential.py
    Added exponential fitting to bin over all pixels, adjusted plotting code.

--- exps/exponential.py
+++ exps/exponential.py
@@ -1,12 +1,9 @@
 import numpy as np
-import time
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-from multiprocessing import Pool
 from skimage.io import imsave, imread
-from scipy.io import savemat
-from scipy.signal import convolve, deconvolve
+from scipy.signal import deconvolve
 
 '''
 Script to fit deconvolved exponentials accross a widefield FLIM image and display the full results. 
@@ -16,81 +13,132 @@ To-do:
 '''
 
 # define parameters
-filename = '240604/240604_10ms_adjusted.tif'
-thresh = 10000
-gate_step = 0.09 # gate step size in ns
+filename = '240613_SPAD-QD-10MHz-3f-5000g-10ms-5ns-18ps-18ps-150uW' # no .tif
+gate_num = 5000 # number of gate steps used
+gate_step = 0.018 # gate step size in ns
+thresh = 2500 # threshold for sum of counts over trace, below which there is no fitting done
+
+freq = 10 # frequency in MHz
+frames = 3 # number of frames
+gate_integ = 100 # integration time in ns
 gate_width = 5 # gate width in ns
-gate_num = 1000 # number of gates for a given step
-gate_offset = 0.018 # gate offset in ns
-irf_width = 0
+gate_offset = 0.018 # gate offset in ps
+irf_width = 0 # width of gaussian irf in ns
+
 
 # exponential decay helper function
 def decay(x, amp, tau):
-        return amp * np.exp(-x / tau)
+  return amp * np.exp(-x / tau)
 
 # scipy.curve_fit wrapper
 def fit_decay(times, data):
-        initial_guess = [np.max(data), 2.0] # use max for the amplitude, choice is ultimately insignificant with LMA algorithm
-        params, cov = opt.curve_fit(decay, times, data, p0=initial_guess)
-        return params
+  initial_guess = [np.max(data), 2.0] # use max for the amplitude, choice is ultimately insignificant with LMA algorithm
+  params, cov = opt.curve_fit(decay, times, data, p0=initial_guess)
+  return params
 
 # deconvolution function
 def irf_deconvolve(times, trace, irf_width):
-    irf = np.exp(-times**2 / irf_width)
-    tracedc = deconvolve(trace, irf)
-    return tracedc
+  irf = np.exp(-times**2 / irf_width)
+  tracedc = deconvolve(trace, irf)
+  return tracedc
 
-# read image and set initial arrays
-image = imread(filename)
-length, x, y = np.shape(image)
-times = (np.arange(length) * gate_step) + gate_offset
-A = np.zeros((x, y))
-intensity = np.zeros((x, y))
-tau = np.zeros((x, y))
+def fit_exps(filename, freq, frames, 
+  gate_num, gate_integ, gate_width, 
+  gate_step, gate_offset, irf_width, thresh):   
+  # read image and set initial arrays
+  image = imread(filename + '.tif')
+  length, x, y = np.shape(image)
 
-# fit exponentials and save info
-for i in range(x):
+  times = (np.arange(gate_num) * gate_step) + gate_offset
+  A = np.zeros((x, y))
+  intensity = np.zeros((x, y))
+  tau = np.zeros((x, y))
+  full_trace = np.zeros((gate_num))
+  track = 0
+
+  # fit exponentials and save info
+  for i in range(x):
     for j in range(y):
-        trace = image[:, i, j]
-        if (sum(trace) > thresh):
-            intensity[i][j] += sum(trace)
-            loc = np.argmax(trace) # only fit from exponential peak onwards
+      trace = image[:gate_num, i, j]
+      if (np.sum(trace) > thresh):
+        full_trace += trace
+        intensity[i][j] += sum(trace)
+
+        loc = np.argmax(trace) # only fit from exponential peak onwards
+        try: 
             params = fit_decay(times[loc:], trace[loc:])
-            A[i][j] += params[0]
-            tau[i][j] += params[1]
-            print(f'Lifetime at pixel {i}, {j}: {params[1]}')
+            track += 1
+        except RuntimeError:
+            params = [0, 0] # not really sure how else to handle curve_fit failing after many iterations
+        
+        A[i][j] += params[0]
+        tau[i][j] += params[1]
+    
+  loc = np.argmax(full_trace)
+  try: 
+    params = fit_decay(times[loc:], full_trace[loc:])
+  except RuntimeError:
+    params = [0, 0]
 
-            # plt.figure(figsize=(6, 4))
-            # plt.scatter(times, trace, s=5, label='Data')
-            # plt.plot(times, decay(times, params[0], params[1]), label='Fit: tau = {:.2f}'.format(params[1]), color='black')
-            # plt.xlabel('Time, ns')
-            # plt.ylabel('Counts')
-            # plt.legend()
-            # plt.title('Simulated Decay for 1 ms integration, 90 ps step')
-            # plt.show()
+  print(str(track) + ' pixels successfully fit')
+  return A, intensity, tau, times, full_trace, params
 
-# plot code taken from replot.py
-for i in range(len(tau)):
-  for j in range(len(tau[0])):
-    if tau[i][j] > 1000:
-      tau[i][j] = 0
-    if tau[i][j] < -1000:
-       tau[i][j] = 0
+def plot_all(tau, A, intensity, gate_integ, gate_step, times, full_trace, params, filename):
+  for i in range(len(tau)):
+    for j in range(len(tau[0])):
+      if tau[i][j] > 1000:
+        tau[i][j] = 0
+      if tau[i][j] < -1000:
+        tau[i][j] = 0
 
-fig,ax=plt.subplots(1,3,figsize=(9,3),sharex=True,sharey=True)
-im1 = ax[0].imshow(A,cmap='plasma')
-im2 = ax[1].imshow(intensity,cmap='gray')
-im3 = ax[2].imshow(tau,cmap='hsv')
-ax[0].set_title('A')
-ax[1].set_title('Intensity')
-ax[2].set_title(r'$\tau$')
+  for i in range(len(A)):
+    for j in range(len(A[0])):
+      if A[i][j] > 100:
+        A[i][j] = 0
+      if A[i][j] < 0:
+        A[i][j] = 0
 
-for axi in ax.ravel():
+  fig,ax=plt.subplots(2,2,figsize=(9,9))
+
+  im1 = ax[0, 0].imshow(A,cmap='plasma')
+  ax[0, 0].set_title('Amplitudes')
+  plt.colorbar(im1,ax=ax[0, 0],label='cts')
+
+  colors = [(1, 0, 0)] + [(i, i, i) for i in np.linspace(0, 1, 255)]
+  custom = mcolors.LinearSegmentedColormap.from_list('custom_gray', colors, N=256)
+  im2 = ax[0, 1].imshow(intensity,cmap=custom)
+  ax[0, 1].set_title('Intensity')
+  plt.colorbar(im2,ax=ax[0, 1],label='cts')
+
+  ax[1, 0].set_title('Lifetimes')
+  im3 = ax[1, 0].imshow(tau,cmap='hsv')
+  plt.colorbar(im3, ax=ax[1, 0], label='ns')
+  im3.set_clim(6, 20)
+
+  ax[1, 1].set_title('Fully binned trace')
+  ax[1, 1].scatter(times, full_trace, s=5)
+  ax[1, 1].plot(times, decay(times, params[0], params[1]), label='Fit: tau = {:.2f}'.format(params[1]), color='black')
+  ax[1, 1].set_xlabel('Time, ns')
+  ax[1, 1].set_ylabel('Counts')
+  ax[1, 1].tick_params(axis='x', which='both', bottom=True, top=True) # not working 
+  ax[1, 1].tick_params(axis='y', which='both', left=True, right=True)
+  ax[1, 1].legend()
+
+
+  for axi in ax.ravel():
     axi.set_xticks([])
     axi.set_yticks([])
 
-plt.colorbar(im1,ax=ax[0],label='cts')
-plt.colorbar(im2,ax=ax[1],label='cts')
-plt.colorbar(im3,ax=ax[2],label='ns')
-plt.tight_layout()
-plt.show()
+  plt.tight_layout()
+  plt.savefig(filename + 'results.png')
+
+  plt.show()
+  
+def run_exps(filename, freq, frames, gate_num, gate_integ, gate_width, gate_step, gate_offset, irf_width, thresh):
+  A, intensity, tau, times, full_trace, params = fit_exps(filename, freq, frames, 
+  gate_num, gate_integ, gate_width, 
+  gate_step, gate_offset, irf_width, thresh)
+
+  plot_all(tau, A, intensity, gate_integ, gate_step, times, full_trace, params, filename)
+
+run_exps(filename, freq, frames, gate_num, gate_integ, gate_width, gate_step, gate_offset, irf_width, thresh)
