b6960cf46eed1cbd7e54aa9d41e4a901bc408114
Ishaan Singh <sing1125@purdue.edu>
2024-06-27 15:58:46 -0400

    Make units consistent

--- SPAD512/mains/run_exponential.py
+++ SPAD512/mains/run_exponential.py
@@ -6,10 +6,34 @@ import time
 from SPAD512.exps import Fitter, Plotter
 from SPAD512.utils import GatedReader
 
-read = False # read from folder (if read=True, file var below is irrelevant)
+'''
+Script for exponential analyses of time-gated FLIM data. .json formatting info below.
+
+    "freq": MHz, pulsed laser frequency
+    "frames": counts, number of decay profiles acquired
+    "numsteps": counts, number of gate step increments
+    "integ": us, exposure time for a single gate
+    "width": ns, width of a single gate
+    "step": ps, increment size between gates
+    "offset": ps, initial increment for first gate step"freq": 10,
+    "power": uW, laser power
+    "bits": counts, bit depth of acquisition
+    "globstrs_1bit": [], 1-bit image info
+    "folder": str, name and path of folder holding images to stack
+    "roi_dim": count, size of square to extract as ROI from top left
+        Note: All entries power --> roi_dim unneeded if not reading from .tif
+    "thresh": counts, sum of trace over pixel needed to not discard pixel in analysis
+    "irf_mean": ns, mean value of gaussian IRF
+    "irf_width": ns, stdev of gaussian IRF
+    "fit": ('mono', 'mono_conv', 'log_mono_conv', 'mh_mono_conv', 'bi'), analysis method to use
+'''
+
+read = False # read from folder (if read=False, must specify a file below)
 file = 'mains\\240613_SPAD-QD-10MHz-3f-900g-1000us-5ns-100ps-18ps-150uW' # make sure to remove .tif suffix
 config_path = 'mains\\run_exponential.json' # no leading slash, keep .json suffix
 show = True # show final plot
+
+
 
 class Analyzer:
     def __init__(self, config_path):
@@ -17,7 +41,7 @@ class Analyzer:
             self.config = json.load(f)
 
     def run(self, show=True, read=False, file=None):
-        if (read==True):
+        if read:
             data = GatedReader(self.config)
             filename = data.create_data()
             print(f"Data created: {filename}")
@@ -39,9 +63,3 @@ class Analyzer:
 if __name__=='__main__':
     info = Analyzer(config_path)
     info.run(show=show, read=read, file=file)
-
-# utils/read from folder
-# option for utils/rebin
-# exps/thresh
-# exps/fit
-# exps/plot
