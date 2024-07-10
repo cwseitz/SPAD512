Fix convolution normalization

SPAD512/mains/run_simulation.py
@@ -51,6 +51,7 @@ class Simulator:
             for j, step in enumerate(self.orig_steps):
                 tic = time.time()
                 dt = Generator(self.config, integ=integ, step=step)
+                dt.plotTrace()
                 dt.genImage()
                 toc = time.time()
                 print(f'Data for {(integ * 1e-3):.3f} ms integ, {(step * 1e-3):.3f} ns step generated in {(toc-tic):.1f} seconds')
@@ -69,7 +70,7 @@ class Simulator:
                     self.stdevs[1,i,j] += np.std(nonzero)
 
                 toc = time.time()
-                print(f'Data analyzed in {(toc-tic):.1f} seconds. Mean lifetimes {(self.means[0,i,j]):.2f} ns, {(self.means[1,i,j]):.2f} ns. \n')
+                print(f'Data analyzed in {(toc-tic):.1f} seconds. Mean lifetimes {(self.means[0,i,j]):.2f} ns \n')
         np.savez(self.config['filename'] + '_results.npz', means=self.means, stdevs=self.stdevs)
          
         
@@ -104,4 +105,4 @@ class Simulator:
 if __name__ == '__main__':
     obj = Simulator(config_path)
     obj.run_full()
-    obj.plot_sim(show=show)
+    # obj.plot_sim(show=show)

SPAD512/utils/deconvolve_fourier.py
@@ -44,7 +44,7 @@ tau = 10
 decay = 100* np.exp(-time / tau)
 decay /= np.sum(decay)
 
-irf_width = 3
+irf_width = 1
 irf_mean = 10
 irf = gaussian(time, irf_mean, irf_width)
 irf /= np.sum(irf)  # normalize
@@ -65,11 +65,10 @@ normed = deconvolved/(np.sum(deconvolved))
 
 
 
-plt.plot(time, decay, label='Original Trace')
+plt.plot(time, decay, label='Original Trace, tau = 10 ns')
 plt.plot(time, detected, label='Convolved Signal')
-plt.plot(time, deconvolved, label='Deconvolved Signal')
-plt.plot(time, irf, label='IRF')
-plt.plot(time, normed, label='Normalized Deconvolved')
+# plt.plot(time, irf, label='IRF, N(10, 1)')
+plt.plot(time, normed, label='Deconvolved Signal')
 plt.xlabel('Time (ns)')
 plt.ylabel('Intensity')
 plt.legend()

SPAD512/utils/gen.py
@@ -48,28 +48,27 @@ class Generator:
         for i, lt in enumerate(self.tau):
             lam = 1/lt
             prob[i,:] += self.zeta * (np.exp(-lam * (self.offset + steps)) - np.exp(-lam * (self.offset + steps + self.width)))
-    
+            if convolve:
+                prob[i,:] = self.convolveProb(prob[i,:])
+
         for i in range(self.numsteps):
             comp =  np.random.choice(np.arange(len(self.tau)))
             draws = np.random.rand(numgates) < prob[comp,i]
             data[i] = np.sum(draws)
 
-        if convolve:
-            data_conv = self.convolveTrace(data)
+        return data
 
-        return data_conv
-
-    def convolveTrace(self, trace):
-        newtrace = trace
+    def convolveProb(self, trace):
         irf_mean = self.config['irf_mean']
         irf_width = self.config['irf_width']
+        irf = np.exp(-((self.times - irf_mean)**2) / (2 * irf_width**2))
+        irf /= (irf_width * np.sqrt(2*np.pi))
 
-        irf = np.exp(-((self.times - irf_mean)**2) / (2 * irf_width**2)) / (irf_width * np.sqrt(2*np.pi))
-        irf /= irf.sum()
+        irf /= np.sum(irf) # discrete normalization
 
-        detected = convolve(newtrace, irf, mode='full')    
+        detected = convolve(trace, irf, mode='full')   
 
-        return detected[:len(trace)]
+        return detected[:len(trace)] 
 
     def plotTrace(self):
         data = self.genTrace()