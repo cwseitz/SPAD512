ca8d4af33946154e6a1bd0672f2d23bf0ea3e435
Ishaan Singh <sing1125@purdue.edu>
2024-08-14 12:39:32 -0400

    Sigma linspace

--- SPAD512/utils/test_rld.py
+++ SPAD512/utils/test_rld.py
@@ -4,6 +4,9 @@ from scipy.signal import convolve, deconvolve
 from scipy.integrate import quad
 import random
 
+import warnings
+warnings.simplefilter("ignore", category=RuntimeWarning)
+
 def bi_exp(x, A1, t1, A2, t2):
     return A1 * np.exp(-x / t1) + A2 * np.exp(-x / t2)
 
@@ -38,42 +41,67 @@ def perturb(data, sigma=0.1):
     data += data * np.random.normal(0, sigma, size=data.shape)
     return data
 
-def run_single(params, g, s, off):
+def run_single(params, g, s, off, sigma=0.1, factor=1000):
     A1, t1, A2, t2 = params
 
     regs = [(off, off+g), (off+s, off+g+s), (off+2*s, off+g+2*s), (off+3*s, off+g+3*s)]
     data = []
 
     for reg in regs:
-        counts = integrate(reg, A1, t1, A2, t2)
+        counts = integrate(reg, A1, t1, A2, t2, factor=factor)
         data.append(counts)
 
-    data = perturb(data)
+    data = perturb(data, sigma=sigma)
 
     n_A1, n_t1, n_A2, n_t2 = bi_rld(data, g, s)
     results = [(t1-n_t1)/t1, (t2-n_t2)/t2]
     norm_t = np.linalg.norm(np.array(results))
     
-    return(norm_t)
+    # return norm_t
+    return(n_t1, n_t2)
 
 params = [1, 5, 2, 20]
-g_sims = [5, 6, 7, 8, 9, 10]
-s_sims = [1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 4.25, 4.5, 4.75, 5]
+g_sims = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+s_sims = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+iters = 10
+sigmas = np.linspace(0.0001, 0.1, num=1000)
 off = 0
 
 finals = []
-avg = np.zeros((len(g_sims), len(s_sims)))
+slopes = np.zeros((len(g_sims), len(s_sims)))
+tau1 = np.zeros(iters)
+tau2 = np.zeros(iters)
 for i, g in enumerate(g_sims):
     for j, s in enumerate(s_sims):
         print(f'Testing gate {g} ns and step {s} ns')
-        for k in range(10):
-            finals.append(run_single(params, g, s, off))
-        avg[i,j] += np.nanmean(np.array(finals))
-        print(avg[i,j])
+        results = np.zeros(len(sigmas))
+        for k, sigma in enumerate(sigmas):
+            for l in range(iters):
+                tau1[l], tau2[l] = run_single(params, g, s, off, sigma=sigma, factor=10)
+            results[k] = np.nanstd(tau1)/(params[1])
+
+        mask = ~np.isnan(results)
+        results = results[mask]
+        sigmas = sigmas[mask]
+
+        q1 = np.percentile(results, 25)
+        q3 = np.percentile(results, 75)
+        iqr = q3-q1
+        lower = q1 - 1.5*iqr
+        upper = q3 + 1.5*iqr
+
+        indices = np.where((results < lower) | (results > upper))
+        reg_sigmas = np.delete(sigmas, indices)
+        reg_results = np.delete(results, indices)
+
+        slope, intercept = np.polyfit(reg_sigmas, reg_results, 1)
+
+        slopes[i,j] = slope
+        print(slope)
         print('')
 
 fig, ax = plt.subplots()
-cax = ax.imshow(avg)
+cax = ax.imshow(slopes)
 ax.set_title('Delta value over various gate and step sizes')
 ax.set_ylabel('Gate length (ns)')
 ax.set_xlabel('Step size (ns)')
@@ -83,5 +111,17 @@ ax.set_xticks(np.linspace(0, len(s_sims), num=len(s_sims), endpoint=False))
 ax.set_xticklabels(s_sims)
 fig.colorbar(cax)
 plt.show()
+
+# fig, ax = plt.subplots()
+# cax = ax.imshow(avg)
+# ax.set_title('Delta value over various gate and step sizes')
+# ax.set_ylabel('Gate length (ns)')
+# ax.set_xlabel('Step size (ns)')
+# ax.set_yticks(np.linspace(0, len(g_sims), num=len(g_sims), endpoint=False))
+# ax.set_yticklabels(g_sims)
+# ax.set_xticks(np.linspace(0, len(s_sims), num=len(s_sims), endpoint=False))
+# ax.set_xticklabels(s_sims)
+# fig.colorbar(cax)
+# plt.show()
     
 
