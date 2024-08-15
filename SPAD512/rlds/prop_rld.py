0f8812cef6fa19242c398811ca51b1cd4e88f95b
Ishaan Singh <sing1125@purdue.edu>
2024-08-15 11:02:02 -0400

    Test

--- SPAD512/rlds/prop_rld.py
+++ SPAD512/rlds/prop_rld.py
@@ -34,40 +34,72 @@ def gen(reg, params, numgates=1000000, iter=10000, zeta=0.01):
 
     return np.random.binomial(numgates, prob, size=iter)
 
+def hist_single(off, g, s, params):
+    off = 0
+    g = 5
+    s = 24
+    params = [1, 5, 2, 20]
+    gates = [(off + i * s, off + g + i * s) for i in range(4)]
+    data = []
+    for i, gate in enumerate(gates):
+        data.append(gen(gate, params))
+
+    n_A1, n_tau1, n_A2, n_tau2 = bi_rld(data, g, s)
+
+    plt.hist(n_tau1, bins=50,  density=True)
+    plt.title(f'Distribution of end values for {g} ns gate length and {s} ns step size')
+    plt.xlabel('Tau 1')
+    plt.xlim(0, 100)
+    plt.ylabel('PDF')
+    plt.show()
+
+def cmap_all(off, g_sims, s_sims, params, thresh=0.2):
+    successes = np.zeros((len(g_sims), len(s_sims), 2))
+    for j, g in enumerate(g_sims):
+        for k, s in enumerate(s_sims):
+            gates = [(off + i * s, off + g + i * s) for i in range(4)]
+            data = []
+            for i, gate in enumerate(gates):
+                data.append(gen(gate, params))
+
+            n_A1, n_tau1, n_A2, n_tau2 = bi_rld(data, g, s)
+            successes[j,k,0] += np.sum((n_tau1 >= params[1]-thresh*params[1]) & (n_tau1 <= params[1]+thresh*params[1])) / (len(data[0]))
+            successes[j,k,1] += np.sum((n_tau2 >= params[3]-thresh*params[3]) & (n_tau2 <= params[3]+thresh*params[3])) / (len(data[0]))
+
+    fig, ax = plt.subplots(1,2)   
+    plt.suptitle(f'Proportion of samples with lifetimes within {100*thresh} % error')
+
+    cax1 = ax[0].imshow(successes[:,:,0])
+    ax[0].set_ylabel('Gate length (ns)')
+    ax[0].set_xlabel('Step size (ns)')
+    ax[0].set_yticks(np.linspace(0, len(g_sims), num=len(g_sims), endpoint=False))
+    ax[0].set_yticklabels(g_sims)
+    ax[0].set_xticks(np.linspace(0, len(s_sims), num=len(s_sims), endpoint=False))
+    ax[0].set_xticklabels(s_sims)
+    fig.colorbar(cax1)
+
+    cax2 = ax[1].imshow(successes[:,:,1])
+    ax[1].set_ylabel('Gate length (ns)')
+    ax[1].set_xlabel('Step size (ns)')
+    ax[1].set_yticks(np.linspace(0, len(g_sims), num=len(g_sims), endpoint=False))
+    ax[1].set_yticklabels(g_sims)
+    ax[1].set_xticks(np.linspace(0, len(s_sims), num=len(s_sims), endpoint=False))
+    ax[1].set_xticklabels(s_sims)
+    fig.colorbar(cax2)
+
+    plt.show()
+
+    plt.hist(n_tau1, bins=50,  density=True)
+    plt.title(f'Distribution of end values for {g} ns gate length and {s} ns step size')
+    plt.xlabel('Tau 1')
+    plt.xlim(0, 100)
+    plt.ylabel('PDF')
+    plt.show()
+
 off = 0
 g_sims = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 s_sims = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 params = [1, 5, 2, 20]
-thresh = 0.5
+thresh = 0.2
 
-successes = np.zeros((len(g_sims), len(s_sims)))
-for j, g in enumerate(g_sims):
-    for k, s in enumerate(s_sims):
-        gates = [(off + i * s, off + g + i * s) for i in range(4)]
-        data = []
-        for i, gate in enumerate(gates):
-            data.append(gen(gate, params))
-
-        n_A1, n_tau1, n_A2, n_tau2 = bi_rld(data, g, s)
-        successes[j,k] += np.sum((n_tau1 >= params[1]-thresh*params[1]) & (n_tau1 <= params[1]+thresh*params[1]))
-        successes[j,k] += np.sum((n_tau2 >= params[3]-thresh*params[3]) & (n_tau2 <= params[3]+thresh*params[3]))
-        successes[j,k] /= (2*len(data[0]))
-
-fig, ax = plt.subplots()
-cax = ax.imshow(successes)
-ax.set_title(f'Proportion of samples with lifetimes within {thresh} % error')
-ax.set_ylabel('Gate length (ns)')
-ax.set_xlabel('Step size (ns)')
-ax.set_yticks(np.linspace(0, len(g_sims), num=len(g_sims), endpoint=False))
-ax.set_yticklabels(g_sims)
-ax.set_xticks(np.linspace(0, len(s_sims), num=len(s_sims), endpoint=False))
-ax.set_xticklabels(s_sims)
-fig.colorbar(cax)
-plt.show()
-
-# plt.hist(n_tau1, bins=50,  density=True)
-# plt.title(f'Distribution of end values for {g} ns gate length and {s} ns step size')
-# plt.xlabel('Tau 1')
-# plt.xlim(0, 100)
-# plt.ylabel('PDF')
-# plt.show()
+cmap_all(off, g_sims, s_sims, params, thresh=0.2)
