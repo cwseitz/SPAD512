3d0f3fa4baf51630670be85f18f4631191cfd13d
Ishaan Singh <sing1125@purdue.edu>
2024-11-12 16:00:55 -0500

    Start blackbox

--- SPAD512/bayes/hamilton.py
+++ SPAD512/bayes/hamilton.py
@@ -3,9 +3,9 @@ import numpy as np
 import arviz as az
 import scipy.special as sp
 import matplotlib.pyplot as plt
-import aesara
-import aesara.tensor as at
-import IPython
+from pytensor.graph import Apply, Op
+import pytensor as pt
+from scipy.optimize import approx_fprime
 
 '''ground truth values (not known)'''
 lam1 = 1/20
@@ -58,25 +58,128 @@ def gen(K, numsteps, step, offset, width, tau_irf, sigma_irf, A, B, lam1, lam2, 
         data.append(np.random.binomial(K, P_tot))
 
     return data
-data = gen(K, numsteps, step, offset, width, tau_irf, sigma_irf, A, B, lam1, lam2, chi)
 
 
-
-'''actually relevant code'''
-with pm.Model() as model:
-    A = pm.HalfNormal('A', sigma=0.2)
-    B = pm.Beta('B', alpha=1, beta=1)
-    lam1 = pm.Gamma('lam1', alpha=1, beta=1)
-    lam2 = pm.Gamma('lam2', alpha=1, beta=1)
-    chi = pm.Exponential('chi', lam=1000)
-
-    P_chi = pm.math.exp(-chi)
-
-    # this is the annoying part, need to blackbox the likelihood calculation because its ugly
-    P_tot = [P_i(offset + (i-1)*step, offset + (i-1)*step + width, A, B, lam1, lam2, tau_irf, sigma_irf) + P_chi for i in range(numsteps)]
-    y_like = [pm.Binomial(f"y_{i}", n=K, p=P_tot[i], observed = data[i]) for i in range(numsteps)]
-
-    # only continuous variables are input so it should default to NUTS
-    trace = pm.sample(1000, tune=500, target_accept=0.9, chains=1)
+# data = gen(K, numsteps, step, offset, width, tau_irf, sigma_irf, A, B, lam1, lam2, chi)
 
 
+'''working through pymc's tutorial on blackboxing'''
+def my_model(m, c, x):
+    return m * x + c
+
+def my_loglike(m, c, sigma, x, data):
+    model = my_model(m, c, x)
+    return -0.5 * ((data - model) / sigma) ** 2 - np.log(np.sqrt(2 * np.pi)) - np.log(sigma)
+
+def finite_differences_loglike(m, c, sigma, x, data, eps=1e-7):
+    def inner_func(mc, sigma, x, data):
+        return my_loglike(*mc, sigma, x, data)
+    
+    grad_wrt_mc = approx_fprime([m, c], inner_func, [eps, eps], sigma, x, data)
+    
+    return grad_wrt_mc[:,0], grad_wrt_mc[:,1]
+
+class LogLikeWithGrad(Op):
+    def make_node(self, m, c, sigma, x, data) -> Apply:
+        # same as before
+        m = pt.tensor.as_tensor_variable(m)
+        c = pt.tensor.as_tensor_variable(c)
+        sigma = pt.tensor.as_tensor_variable(sigma)
+        x = pt.tensor.as_tensor_variable(x)
+        data = pt.tensor.as_tensor_variable(data)
+
+        inputs = [m, c, sigma, x, data]
+        outputs = [data.type()]
+        return Apply(self, inputs, outputs)
+
+    def perform(self, node: Apply, inputs: list[np.ndarray], outputs: list[list[None]]) -> None:
+        # also same
+        m, c, sigma, x, data = inputs  
+        loglike_eval = my_loglike(m, c, sigma, x, data)
+        outputs[0][0] = np.asarray(loglike_eval)
+
+    def grad(self, inputs: list[pt.tensor.TensorVariable], g: list[pt.tensor.TensorVariable]
+    ) -> list[pt.tensor.TensorVariable]:
+        # this will return the vector jacobian product for gradient
+        m, c, sigma, x, data = inputs
+        if m.type.ndim != 0 or c.type.ndim != 0:
+            raise NotImplementedError("Graident only implemented for scalar m and c")
+        
+        grad_wrt_m, grad_wrt_c = loglikegrad_op(m, c, sigma, x, data)
+
+        # out_grad is a tensor of gradients of the Op outputs wrt to the function cost
+        [out_grad] = g
+        return [
+            pt.tensor.sum(out_grad * grad_wrt_m),
+            pt.tensor.sum(out_grad * grad_wrt_c),
+            # the other 3 inputs still need a gradient but we dont need it because they are model constants
+            pt.gradient.grad_not_implemented(self, 2, sigma), # THIS DOESNT WORK AAAAAAAAAAAAAAAAAAAA
+            pt.gradient.grad_not_implemented(self, 3, x), # THIS DOESNT WORK AAAAAAAAAAAAAAAAAAAA
+            pt.gradient.grad_not_implemented(self, 4, data), # THIS DOESNT WORK AAAAAAAAAAAAAAAAAAAA
+        ]
+
+class LogLikeGrad(Op):
+    def make_node(self, m, c, sigma, x, data) -> Apply:
+        m = pt.tensor.as_tensor_variable(m)
+        c = pt.tensor.as_tensor_variable(c)
+        sigma = pt.tensor.as_tensor_variable(sigma)
+        x = pt.tensor.as_tensor_variable(x)
+        data = pt.tensor.as_tensor_variable(data)
+
+        inputs = [m, c, sigma, x, data]
+        outputs = [data.type(), data.type()]
+
+        return Apply(self, inputs, outputs)
+
+    def perform(self, node: Apply, inputs: list[np.ndarray], outputs: list[list[None]]) -> None:
+        m, c, sigma, x, data = inputs
+
+        grad_wrt_m, grad_wrt_c = finite_differences_loglike(m, c, sigma, x, data)
+
+        outputs[0][0] = grad_wrt_m
+        outputs[1][0] = grad_wrt_c
+
+loglikewithgrad_op = LogLikeWithGrad()
+loglikegrad_op = LogLikeGrad()
+
+if __name__ == '__main__':
+    N = 10 
+    sigma = 1
+    x = np.linspace(0, 9, N)
+    mtrue = 0.4
+    ctrue = 3
+    truemodel = my_model(mtrue, ctrue, x)
+
+    rng = np.random.default_rng(716743)
+    data = sigma * rng.normal(size = N) + truemodel
+
+    # print(finite_differences_loglike(mtrue, ctrue, sigma, x, data))
+
+    # dont mess with order of parameters
+    def custom_dist_loglike(data, m, c, sigma, x):
+        return loglikewithgrad_op(m, c, sigma, x, data)
+    
+    m_symb = pt.tensor.scalar("m")
+    c_symb = pt.tensor.scalar("c")
+
+    # Get the log-likelihood output and gradient
+    logp_output = loglikewithgrad_op(m_symb, c_symb, sigma, x, data)
+    grad_wrt_m, grad_wrt_c = pt.gradient.grad(logp_output.sum(), wrt=[m_symb, c_symb])
+
+    print("Gradient with respect to m:", grad_wrt_m.eval({m_symb: mtrue, c_symb: ctrue}))
+    print("Gradient with respect to c:", grad_wrt_c.eval({m_symb: mtrue, c_symb: ctrue}))
+
+    with pm.Model() as grad_model:
+        m = pm.Uniform("m", lower=-10.0, upper=10.0)
+        c = pm.Uniform("c", lower=-10.0, upper=10.0)
+
+        # use custom distribution to implement custom likelihood
+        likelihood = pm.CustomDist(
+            "likelihood", m, c, sigma, x, observed=data, logp=custom_dist_loglike
+        )
+
+        with grad_model:
+            idata_grad = pm.sample(step=pm.NUTS())
+
+        az.plot_trace(idata_grad, lines=[("m", {}, mtrue), ("c", {}, ctrue)])
+        plt.show()
