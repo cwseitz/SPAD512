ae7d8af12d70bf1d7376418aa5188ddad4b3775e
Ishaan Singh <sing1125@purdue.edu>
2024-11-13 13:08:56 -0500

    Properly implement HMC blackbox

--- SPAD512/bayes/hamilton.py
+++ SPAD512/bayes/hamilton.py
@@ -28,9 +28,24 @@ def h(t, tau_irf, sigma_irf, B, lam1, lam2):
             * sp.erfc((tau_irf - t - lam2 * sigma_irf**2) / (sigma_irf * np.sqrt(2)))
     return term1 + term2
 
-def P_i(start, end, A, B, lam1, lam2, tau_irf, sigma_irf):
+def dh_dB(t, tau_irf, sigma_irf, B, lam1, lam2):
+    term1 = 1 * lam1 * np.exp(lam1 * (tau_irf - t) + 0.5 * lam1**2 * sigma_irf**2) \
+            * sp.erfc((tau_irf - t - lam1 * sigma_irf**2) / (sigma_irf * np.sqrt(2)))
+    term2 = -1 * lam2 * np.exp(lam2 * (tau_irf - t) + 0.5 * lam2**2 * sigma_irf**2) \
+            * sp.erfc((tau_irf - t - lam2 * sigma_irf**2) / (sigma_irf * np.sqrt(2)))
+    return term1 + term2
+
+def dh_dlam(t, tau_irf, sigma_irf, B, lam1, lam2):
+    outer = B * np.exp((lam1*(sigma_irf**2) - 2*t + 2*tau_irf)/2)
+    term1 = lam1 * sigma_irf * np.sqrt(2/np.pi) * np.exp((-(lam1 * sigma_irf**2 + t - tau_irf)**2)/(2 * sigma_irf**2))
+    term2 = (1 + (lam1**2)*(sigma_irf**2) + lam1*(tau_irf - t)) \
+            * sp.erfc(-(lam1 * sigma_irf**2 + t - tau_irf)/(np.sqrt(2)*sigma_irf))
+    return outer * (term1 + term2)
+
+def P_i(start, end, A, B, lam1, lam2, tau_irf, sigma_irf, h_func):
     t_vals = np.linspace(start, end, 100)  # for trapezioidal sum, quad integration probably not needed
-    h_vals = h(t_vals, tau_irf, sigma_irf, B, lam1, lam2)
+    h_vals = h_func(t_vals, tau_irf, sigma_irf, B, lam1, lam2)
+    print(f'A: {A}, B: {B}, lam1: {lam1}, lam2: {lam2}')
     return A * np.trapz(h_vals, t_vals)
 
 def gen(K, numsteps, step, offset, width, tau_irf, sigma_irf, A=0.3, B=0.5, lam1=0.05, lam2=0.2, chi=0.0001):
@@ -41,11 +56,9 @@ def gen(K, numsteps, step, offset, width, tau_irf, sigma_irf, A=0.3, B=0.5, lam1
         start = offset + i*step
         end = start + width
 
-        t_vals = np.linspace(start, end, 100)
-        h_vals = h(t_vals, tau_irf, sigma_irf, B, lam1, lam2)
-        P_i = A * np.trapz(h_vals, t_vals)
+        Pi = P_i(start, end, A, B, lam1, lam2, tau_irf, sigma_irf, h)
 
-        P_tot = P_i + P_chi
+        P_tot = Pi + P_chi
 
         data.append(np.random.binomial(K, P_tot))
 
@@ -55,21 +68,63 @@ def gen(K, numsteps, step, offset, width, tau_irf, sigma_irf, A=0.3, B=0.5, lam1
 # data = gen(K, numsteps, step, offset, width, tau_irf, sigma_irf, A, B, lam1, lam2, chi)
 
 
-'''working through pymc's tutorial on blackboxing'''
-def my_model(m, c, x):
-    return m * x + c
+def cal_loglike(lam1, lam2, A, B, chi, data):
+    loglike = 0
 
-def my_loglike(m, c, sigma, x, data):
-    model = my_model(m, c, x)
-    return -0.5 * ((data - model) / sigma) ** 2 - np.log(np.sqrt(2 * np.pi)) - np.log(sigma)
+    for i, yi in enumerate(data):
+        Pi = P_i(offset + i*step, offset + i*step + width,
+            A, B, lam1, lam2, tau_irf, sigma_irf, h
+        )
+        Pchi = 1 - np.exp(-chi)
+        Ptot = Pi + Pchi
 
-def finite_differences_loglike(m, c, sigma, x, data, eps=1e-7):
-    def inner_func(mc, sigma, x, data):
-        return my_loglike(*mc, sigma, x, data)
+        loglike += np.log(sp.comb(K, yi))
+        loglike += yi * np.log(Ptot)
+        loglike += (K-yi) * np.log(1-Ptot)
+        print(f'Pi: {Pi}')
+
+    return loglike     
+
+def grad_main(lam1, lam2, A, B, chi, data):
+    grad_wrt_lam1 = 0
+    grad_wrt_lam2 = 0
+    grad_wrt_A = 0
+    grad_wrt_B = 0
+    grad_wrt_chi = 0
+
+    for i, val in enumerate(data): # iterate over each step individually for now
+        # ptot calculation
+        Pi = P_i(offset + i*step, offset + i*step + width,
+            A, B, lam1, lam2, tau_irf, sigma_irf, h
+        )
+        Pchi = 1 - np.exp(-chi)
+        Ptot = Pi + Pchi
+
+        # chi
+        dP_dchi = np.exp(-chi)
+        grad_wrt_chi += ((val/Ptot) + ((val - K)/(1-Ptot))) * dP_dchi
+
+        # A
+        dP_dA = (Ptot - 1 + np.exp(-chi))/A
+        grad_wrt_A += ((val/Ptot) + ((val - K)/(1-Ptot))) * dP_dA
+
+        # B
+        t_vals = np.linspace(offset + i*step,  offset + i*step + width, 100)  # for trapezioidal sum, quad integration probably not needed
+        h_vals = dh_dB(t_vals, tau_irf, sigma_irf, B, lam1, lam2)
+        dP_dB = A * np.trapz(h_vals, t_vals)
+        grad_wrt_B += ((val/Ptot) + ((val - K)/(1-Ptot))) * dP_dB
+
+        # lam1
+        h_vals = dh_dlam(t_vals, tau_irf, sigma_irf, B, lam1, lam2)
+        dP_dlam1 = A * np.trapz(h_vals, t_vals)
+        grad_wrt_lam1 += ((val/Ptot) + ((val - K)/(1-Ptot))) * dP_dlam1
+
+        # lam2
+        h_vals = dh_dlam(t_vals, tau_irf, sigma_irf, B, lam2, lam1) # just swap lam1/lam2 because there is no lam1 lam2 mixing
+        dP_dlam2 = A * np.trapz(h_vals, t_vals)
+        grad_wrt_lam2 += ((val/Ptot) + ((val - K)/(1-Ptot))) * dP_dlam2
     
-    grad_wrt_mc = approx_fprime([m, c], inner_func, [eps, eps], sigma, x, data)
-    
-    return grad_wrt_mc[:,0], grad_wrt_mc[:,1]
+    return grad_wrt_lam1, grad_wrt_lam2, grad_wrt_A, grad_wrt_B, grad_wrt_chi
 
 class LogLikeWithGrad(Op):
     def make_node(self, lam1, lam2, A, B, chi, data) -> Apply:
@@ -88,13 +143,13 @@ class LogLikeWithGrad(Op):
     def perform(self, node: Apply, inputs: list[np.ndarray], outputs: list[list[None]]) -> None:
         # also same
         lam1, lam2, A, B, chi, data = inputs  
-        loglike_eval = my_loglike(lam1, lam2, A, B, chi, data)
+        loglike_eval = cal_loglike(lam1, lam2, A, B, chi, data)
         outputs[0][0] = np.asarray(loglike_eval)
 
     def grad(self, inputs: list[pt.tensor.TensorVariable], g: list[pt.tensor.TensorVariable]) -> list[pt.tensor.TensorVariable]:
         lam1, lam2, A, B, chi, data = inputs
         if lam1.type.ndim != 0 or lam2.type.ndim != 0 or A.type.ndim != 0 or B.type.ndim != 0 or chi.type.ndim != 0:
-            raise NotImplementedError("Graident only implemented for scalar m and c")
+            raise NotImplementedError("Graident only implemented for scalars")
         
         grad_wrt_lam1, grad_wrt_lam2, grad_wrt_A, grad_wrt_B, grad_wrt_chi = loglikegrad_op(lam1, lam2, A, B, chi, data)
 
@@ -126,8 +181,7 @@ class LogLikeGrad(Op):
     def perform(self, node: Apply, inputs: list[np.ndarray], outputs: list[list[None]]) -> None:
         lam1, lam2, A, B, chi, data = inputs
 
-        # calculate gradients
-        grad_wrt_lam1, grad_wrt_lam2, grad_wrt_A, grad_wrt_B, grad_wrt_chi = finite_differences_loglike(lam1, lam2, A, B, chi, data)
+        grad_wrt_lam1, grad_wrt_lam2, grad_wrt_A, grad_wrt_B, grad_wrt_chi = grad_main(lam1, lam2, A, B, chi, data)
 
         outputs[0][0] = grad_wrt_lam1
         outputs[1][0] = grad_wrt_lam2
@@ -150,9 +204,9 @@ if __name__ == '__main__':
         # 3: define priors and likelihood
         lam1 = pm.Uniform("lam1", lower=0, upper=2)
         lam2 = pm.Uniform("c", lower=0, upper=2)
-        A = pm.Beta("A", )
-        B = pm.Beta("B", )
-        chi = pm.Gamma("chi", )
+        A = pm.Beta("A", alpha=1, beta=1)
+        B = pm.Beta("B", alpha=1, beta=1)
+        chi = pm.Gamma("chi", alpha=2, beta=0.001)
 
         likelihood = pm.CustomDist(
             "likelihood", lam1, lam2, A, B, chi, observed=data, logp=custom_dist_loglike
